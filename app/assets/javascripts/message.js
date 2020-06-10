ab9199f398c887f80cbb2e58a16934057a8974b1
megkato <meg.8.xxx@i.softbank.jp>
2020-06-10 22:09:07 +0900

    add automation

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,25 @@
 $(function(){
-  $(function(){ 
+  // $(function(){ 
     function buildHTML(message){
-     if ( message.image ) {
-       var html =
-        `<div class="message">
-           <div class="upper-message">
-             <div class="upper-message__user-name">
-               ${message.user_name}
-             </div>
-             <div class="upper-message__date">
-               ${message.created_at}
-             </div>
-           </div>
-           <div class="lower-message">
-             <p class="lower-message__content">
-               ${message.content}
-             </p>
-           </div>
-           <img src=${message.image} >
-         </div>`
-       return html;
-     } else {
+      if  (message.image) { `<img class= "lower-message__image" src=${message.image} >` ; 
+      var html = `<div class="message" data-message-id="${message.id}"> 
+      <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
+        </div>
+        <div class="upper-message__date">
+          ${message.date}
+        </div>
+      </div>
+      <div class="lower-meesage">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+        ${image}
+      </div>
+    </div>`
+    return html;
+  }else {
        var html =
         `<div class="message">
            <div class="upper-message">
@@ -62,6 +61,33 @@ $(function(){
     });
     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     return false;
+    
   });
-});
-});
+  var reloadMessages = function () {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
+
+      $.ajax({ 
+        url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
+      })
+      .done(function (messages) {
+        if (messages.length !== 0) { 
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
+  });
+  
